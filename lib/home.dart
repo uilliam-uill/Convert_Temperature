554f36b0c417fa74779842c1deb9f881414abc59
Uilliam Santos de Aquino <126193812+uilliam-uill@users.noreply.github.com>
2024-03-03 01:57:37 -0300

    Add files via upload

--- lib/home.dart
+++ lib/home.dart
@@ -1,13 +1,65 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class Open extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child:
-      Text("Conversos de Temperatura")
+      color: Colors.black,
+      height: double.infinity,
+      width: double.infinity,
+      child:Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            "assets/images/home-image.png",
+            height: 250,
+            width: 250,
+            ),
+
+          Container(
+            height: 20,
+          ),
+
+          Text(
+            "CONVTEMP",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30
+            )
+            ),
+
+            Container(
+              height: 20,
+            ),
+
+          Text(
+            "Converso de temperatura",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 22
+            )
+          ),
+
+            Container(
+              height: 5,
+            ),
+
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.yellow[700]
+            ),
+            onPressed: (){}, 
+            child:
+             Text(
+              "Acessar conversor",
+              style: TextStyle(
+                color: Colors.white
+              ),
+              )
+            )
+        ],
+      )
     );
   }
 }
